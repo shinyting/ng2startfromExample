2d04ac592316fdd1ac22ed5320f07eb347f66d58
administator <administator>
2016-10-26 11:56:50 +0800

    ng2 start part

---
+++ app/app.component.js
@@ -0,0 +1,42 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var core_1 = require('@angular/core');
+var hero_1 = require('./hero');
+var hero_service_1 = require('./hero.service');
+var AppComponent = (function () {
+    function AppComponent(heroService) {
+        this.heroService = heroService;
+        this.title = 'Tour of Heroes';
+        this.heroes = hero_1.Hero[];
+    }
+    AppComponent.prototype.onSelect = function (hero) {
+        this.selectedHero = hero;
+    };
+    AppComponent.prototype.ngOnInit = function () {
+        this.getHeroes();
+    };
+    AppComponent.prototype.getHeroes = function () {
+        var _this = this;
+        this.heroService.getHeroes().then(function (heroes) { return _this.heroes = heroes; });
+    };
+    AppComponent = __decorate([
+        core_1.Component({
+            selector: 'my-app',
+            template: "\n\t\t<h1>{{title}}</h1>\n\t\t<h2>My Heroes</h2>\n\t\t<ul class=\"heroes\">\n\t\t\t<li *ngFor=\"let hero of heroes\" [class.selected]=\"hero === selectedHero\" (click)=\"onSelect(hero)\">\n\t\t\t\t<span class=\"badge\">{{hero.id}}</span> {{hero.name}}\n\t\t\t</li>\n\t\t</ul>\n\t\t<my-hero-detail [hero]=\"selectedHero\"></my-hero-detail>\n\t",
+            styles: ["\n\t\t.selected {\n\t\t\tbackground-color: #CFD8DC !important;\n\t\t\tcolor: white;\n\t\t}\n\t\t.heroes {\n\t\t\tfloat: left;\n\t\t\tmargin: 0 0 2em 0;\n\t\t\tlist-style-type: none;\n\t\t\tpadding: 0;\n\t\t\twidth: 15em;\n\t\t}\n\t\t.heroes li {\n\t\t\tcursor: pointer;\n\t\t\tposition: relative;\n\t\t\tleft: 0;\n\t\t\tbackground-color: #eee;\n\t\t\tmargin: .5em;\n\t\t\tpadding: .3em 0;\n\t\t\theight: 1.6em;\n\t\t\tborder-radius: 4px;\n\t\t}\n\t\t.heroes li.selected:hover {\n\t\t\tbackground-color: #bbd8dc !important;\n\t\t\tcolor: white;\n\t\t}\n\t\t.heroes li:hover {\n\t\t\tcolor: #607d8b;\n\t\t\tbackground-color: #ddd;\n\t\t\tleft: .1em;\n\t\t}\n\t\t.heroes .text {\n\t\t\tposition: relative;\n\t\t\ttop: -3px;\n\t\t}\n\t\t.heroes .badge {\n\t\t\tdisplay: inline-block;\n\t\t\tfont-size: small;\n\t\t\tcolor: white;\n\t\t\tpadding: 0.8em 0.7em 0 0.7em;\n\t\t\tbackground-color: #607d8b;\n\t\t\tline-height: 1em;\n\t\t\tposition: relative;\n\t\t\tleft: -1px;\n\t\t\ttop: -4px;\n\t\t\theight: 1.8em;\n\t\t\tmargin-right: .8em;\n\t\t\tborder-radius: 4px 0 0 4px;\n\t\t}\n\t\t.hero-detail {\n\t\t\tfloat: left;\n\t\t\tmargin-left: 20px;\n\t\t}\n\t"],
+            providers: [hero_service_1.HeroService]
+        }), 
+        __metadata('design:paramtypes', [hero_service_1.HeroService])
+    ], AppComponent);
+    return AppComponent;
+}());
+exports.AppComponent = AppComponent;
+//# sourceMappingURL=app.component.js.map
---
+++ app/mock-heroes.ts
@@ -0,0 +1,24 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[] = [
+	{id: 11, name: 'Mr. Nice'},
+	{id: 12, name: 'Narco'},
+	{id: 13, name: 'Bombasto'},
+	{id: 14, name: 'Celeritas'},
+	{id: 15, name: 'Magneta'},
+	{id: 16, name: 'RubberMan'},
+	{id: 17, name: 'Dynama'},
+	{id: 18, name: 'Dr IQ'},
+	{id: 19, name: 'Magma'},
+	{id: 20, name: 'Tornado'},
+	{id: 21, name: 'Mikella'},
+	{id: 22, name: 'laty'},
+	{id: 23, name: 'thhaht'},
+	{id: 24, name: 'delll'},
+	{id: 25, name: 'rutty'},
+	{id: 26, name: 'rabbit'},
+	{id: 27, name: 'monkey'},
+	{id: 28, name: 'puppy'},
+	{id: 29, name: 'second'},
+	{id: 30, name: 'last'}
+];
